b376d316ec8f14e164af00be8344dd43e5a98567
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2022-05-09 00:59:59 +0100

    Use process.runtimeconfig.json Absolute Path to Set Runtime in CLRImports Module

--- DataProcessing/CLRImports.py
+++ DataProcessing/CLRImports.py
@@ -1,11 +1,12 @@
 # This file is used to import the environment and classes/methods of LEAN.
 # so that any python file could be using LEAN's classes/methods.
+from os.path import join, dirname, realpath
 from clr_loader import get_coreclr
 from pythonnet import set_runtime
 
 # process.runtimeconfig.json is created when we build the DataProcessing Project:
 # dotnet build .\DataProcessing\DataProcessing.csproj
-set_runtime(get_coreclr('process.runtimeconfig.json'))
+set_runtime(get_coreclr(join(dirname(realpath(__file__)), "process.runtimeconfig.json")))
 
 from AlgorithmImports import *
 from QuantConnect.Lean.Engine.DataFeeds import *
